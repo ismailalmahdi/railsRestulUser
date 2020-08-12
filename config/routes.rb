9bdd612c62414820db48db28d5ec7ad15b812e02
Ismail Al.mahdy <ismailalmahdi@me.com>
2020-08-13 07:07:45 +0800

    login and logout is working with token authincation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+    namespace :api, defaults: {format: :json} do
+      namespace :v1 do
+        # posts routes
+        resources :posts
+        # user routes
+        devise_scope :user do
+          post "sign_up", to: "registrations#create"
+          post "sign_in", to: "sessions#create"
+        end
+      end
+    end
 end
